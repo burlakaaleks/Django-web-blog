362fdcc0c049f2be5ec6a792d3f38da43bea8c65
Olexandr Burlaka <wrkttt@gmail.com>
2019-06-12 01:05:50 +0300

    added post model and template

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 def posts_list(request):
-	n = 'Alex'
-	labels = ['Nike', 'Adidas', 'Virginia', 'Marlboro']
-	return render(request, 'blog/index.html', context={'labels':labels, 'name':n})
+	posts = Post.objects.all()
+	name = 'Alex'
+	return render(request, 'blog/index.html', context={'posts':posts, 'name':name})
+
+def post_detail(request, slug):
+	post = Post.objects.get(slug__iexact = slug)
+	return render(request, 'blog/post_detail.html', context = {'post': post})
