55836580905da5f6623c6c961dd6fe0b9e2ddbd3
maskable <wrkttt@gmail.com>
2019-06-15 14:50:12 +0300

    forms.py, creating new tags

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
 
 from .models import *
 from .utils import ObjectDetailMixin
+from .forms import TagForm
 
 # Create your views here.
 def posts_list(request):
@@ -24,12 +25,28 @@ class PostDetail(ObjectDetailMixin, View):
 	# 	post = get_object_or_404(Post, slug__iexact = slug)
 	# 	return render(request, 'blog/post_detail.html', context = {'post': post})
 
+
 class TagDetail(ObjectDetailMixin, View):
 	model = Tag
 	template = 'blog/tag_detail.html'
 	# def get(self, request, slug):
 	# 	tag = Tag.objects.get(slug__iexact=slug)
 	# 	return render(request, 'blog/tag_detail.html', context={'tag':tag})
+
+
+class TagCreate(View):
+	def get(self, request):
+		form = TagForm()
+		return render(request, 'blog/tag_create.html', context={'form':form})
+
+	def post(self, request):
+		bound_form = TagForm(request.POST)
+
+		if bound_form.is_valid():
+			new_tag = bound_form.save()
+			return redirect(new_tag)
+		return render(request, 'blog/tag_create.html', context={'form':bound_form})
+
 
 def tags_list(request):
 	tags = Tag.objects.all()
